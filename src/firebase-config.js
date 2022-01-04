a6112e83635259bc8ea777fc59a2e209263859ed
Ogelo10 <augustineofoegbu1@gmail.com>
2022-01-04 20:47:57 +0100

    React Firebase CRUD app

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9ECQyxsq_xQlnWzUn1DK2YbdbQkG38gI",
+    authDomain: "fir-crud-4a0ac.firebaseapp.com",
+    projectId: "fir-crud-4a0ac",
+    storageBucket: "fir-crud-4a0ac.appspot.com",
+    messagingSenderId: "560271347240",
+    appId: "1:560271347240:web:7f8081402c72aa1f69af1d",
+    measurementId: "G-CW57HHWVGR"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  
+
