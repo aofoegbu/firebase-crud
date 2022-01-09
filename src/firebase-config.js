aca56cd9e99c1df5785fa3fbe9912b46152e3e61
Ogelo10 <augustineofoegbu1@gmail.com>
2022-01-10 00:50:33 +0100

    Reactjs and Firebase CRUD app.

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getFireStore } from "@firebase/firestore";
+import { getFireStore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyD9ECQyxsq_xQlnWzUn1DK2YbdbQkG38gI",
-    authDomain: "fir-crud-4a0ac.firebaseapp.com",
-    projectId: "fir-crud-4a0ac",
-    storageBucket: "fir-crud-4a0ac.appspot.com",
-    messagingSenderId: "560271347240",
-    appId: "1:560271347240:web:7f8081402c72aa1f69af1d",
-    measurementId: "G-CW57HHWVGR"
-  };
+  apiKey: "AIzaSyD9ECQyxsq_xQlnWzUn1DK2YbdbQkG38gI",
+  authDomain: "fir-crud-4a0ac.firebaseapp.com",
+  projectId: "fir-crud-4a0ac",
+  storageBucket: "fir-crud-4a0ac.appspot.com",
+  messagingSenderId: "560271347240",
+  appId: "1:560271347240:web:7f8081402c72aa1f69af1d",
+  measurementId: "G-CW57HHWVGR"
+};
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-  export const db = getFireStore(app);
+export const db = getFireStore(app);
